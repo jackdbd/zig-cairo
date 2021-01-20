4d132beab9ab09d7b4269d3f3622e4d15bc7936b
Giacomo Debidda <jackdebidda@gmail.com>
2021-01-20 17:09:01 +0100

    Move before_install to its own shell script

---
+++ travis_before_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# before_install script to use on Travis CI
+# https://docs.travis-ci.com/user/installing-dependencies/#installing-projects-from-source
+
+set -ex
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    sudo apt-get install -y libcairo2-dev;
+fi
+
+if [[ "$ZIG_VERSION" == "0.8.0-dev.1032+8098b3f84" ]]; then
+    wget https://ziglang.org/builds/zig-linux-x86_64-${ZIG_VERSION}.tar.xz
+else
+    wget https://ziglang.org/download/${ZIG_VERSION}/zig-linux-x86_64-${ZIG_VERSION}.tar.xz
+
+tar -xvf zig-linux-x86_64-${ZIG_VERSION}.tar.xz
+export PATH=$PATH:$PWD/zig-linux-x86_64-${ZIG_VERSION}/
