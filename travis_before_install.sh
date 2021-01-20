c26d5c359e269109682b515d3a22d419b5716bee
Giacomo Debidda <jackdebidda@gmail.com>
2021-01-20 18:59:00 +0100

    Try to fix Travis CI

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -5,18 +5,19 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
+export ZIG_VERSION=0.7.0
+
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt-get install -y libcairo2-dev;
 fi
 
 if [[ $ZIG_VERSION == 0.8.0-dev* ]]; then
-    wget https://ziglang.org/builds/zig-linux-x86_64-"$ZIG_VERSION".tar.xz
+    wget https://ziglang.org/builds/zig-linux-x86_64-"$ZIG_VERSION".tar.xz -O /tmp/zig-linux-x86_64-"$ZIG_VERSION".tar.xz
 else
-    wget https://ziglang.org/download/"$ZIG_VERSION"/zig-linux-x86_64-"$ZIG_VERSION".tar.xz
+    wget https://ziglang.org/download/"$ZIG_VERSION"/zig-linux-x86_64-"$ZIG_VERSION".tar.xz -O /tmp/zig-linux-x86_64-"$ZIG_VERSION".tar.xz
 fi
 
-tar -xvf zig-linux-x86_64-"$ZIG_VERSION".tar.xz && rm zig-linux-x86_64-"$ZIG_VERSION".tar.xz
-mv "$PWD"/zig-linux-x86_64-"$ZIG_VERSION" "$PWD"/zig && mv "$PWD"/ "$HOME"/zig
+tar -xvf /tmp/zig-linux-x86_64-"$ZIG_VERSION".tar.xz -C "$HOME"
+rm /tmp/zig-linux-x86_64-"$ZIG_VERSION".tar.xz
+mv "$HOME"/zig-linux-x86_64-"$ZIG_VERSION" "$HOME"/zig
 export PATH=$PATH:$HOME/zig
-# export PATH=$PATH:$HOME/zig/zig
-cp "$HOME"/zig/zig ${HOME}/bin
